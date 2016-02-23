2be165ef28a317ef271a197b8f521d6589a0f81a
virendergit1 <ch_virender@yahoo.com>
2016-02-22 21:39:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 ﻿<?php  
 $host = $_SERVER["HTTP_HOST"];
 echo $host;
-header( 'Location: https://dkapmstatic.herokuapp.com/dist/src/index.html' ) ;  
+header( 'Location: https://vast-tor-66111.herokuapp.com/dist/src/index.html' ) ;  
 exit;
 ?>
